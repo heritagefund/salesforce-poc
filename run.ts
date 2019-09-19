fb38310450c6c15984a0344607560b50af7d4fcc
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-09-19 11:36:34 +0100

    send to APEX REST endpoint

--- run.ts
+++ run.ts
@@ -1,19 +1,70 @@
-const nforce = require('nforce')
-import './lib/env'
-async function run()  {
+const nforce = require('nforce');
+require("./lib/env");
+const payload = {
+    "meta": {
+        "form": "3-10k-grant",
+        "schemaVersion": "v1.x",
+        "environment": "production",
+        "commitId": "b4ecf18eae01d34b296e9388f387cc42bf7c0f93",
+        "locale": "en",
+        "username": "example@example.com",
+        "applicationId": "e9ae2cc4-fd7b-4fe5-bd55-17317a288fd4",
+        "startedAt": "2019-05-17T15:34:13.000Z"
+    },
+    "application": {
+        "projectName": "My project name",
+        "projectDateRange": {
+            "startDate": "2020-12-12",
+            "endDate": "2020-12-12"
+        },
+        "projectAddress": {
+            "projectPostcode": "B15 1TR",
+            "line1": "82553 Demarco Rapid",
+            "townCity": "Waelchitown",
+            "county": "Berkshire"
+        },
+        "yourIdeaProject": "Free text…",
+        "projectTotalCosts": 20000,
+        "organisationName": "Goldberg Enterprises",
+        "organisationAddress": {
+            "line1": "82553 Demarco Rapid",
+            "townCity": "Waelchitown",
+            "county": "Berkshire",
+            "postcode": "B15 1TR"
+        },
+        "organisationType": "not-for-profit-company",
+        "companyNumber": "123456789",
+        "charityNumber": null,
+        "charityNumberNi": null,
+        "mainContactName": "Nelda",
+        "mainContactDateOfBirth": "1975-10-12",
+        "mainContactAddress": {
+            "line1": "41465 Bashirian Oval",
+            "townCity": "Friesenhaven",
+            "county": "Berkshire",
+            "postcode": "B15 1TR"
+        },
+        "mainContactEmail": "Lizzie87@example.com",
+        "mainContactPhone": "0345 4 10 20 30",
+        "authorisedSignatoryRole": "trustee",
+        "authorisedSignatoryName": "Jane Doe",
+        "authorisedSignatoryPhone": "07777 777777"
+    }
+}
+
+async function run() {
     const org = nforce.createConnection({
         clientId: process.env.SALESFORCE_CONSUMER_KEY,
         clientSecret: process.env.SALESFORCE_CONSUMER_SECRET,
         redirectUri: 'http://localhost:3000/oauth/_callback',
         environment: 'sandbox'
-    })
+    });
 
     return await org.authenticate({
-        username: process.env.SALESFORCE_USERNAME, 
+        username: process.env.SALESFORCE_USERNAME,
         password: process.env.SALESFORCE_PASSWORD
     })
+        .then((oauth: any) => org.apexRest({ oauth: oauth, uri: 'loadData', method: 'POST', body: JSON.stringify(payload) }));
 };
-run().catch(e => {console.log(`error authenticating ${e}`)}).then(oauth => (console.log(oauth)))
 
-
-
+run().catch(e => { console.log(`error from salesforce ${e}`); }).then(console.log);
