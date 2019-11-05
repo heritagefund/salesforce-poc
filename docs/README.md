53257dae306121f42b6d1962ca691f97a9c1f1d6
Gideon Goldberg <gideon.goldberg@heritagefund.org.uk>
2019-11-05 15:40:38 +0000

    delete existing files

--- docs/README.md
+++
@@ -1,7 +0,0 @@
-# Readme
-
-## /
-
-- [3-10k application](./application.schema.md) – `application.schema.json` (Unknown)
-- [Completion Report](./CompletionReport.schema.md) – `CompletionReport.schema.json` (Unknown)
-- [permission to start](./PermissionToStart.schema.md) – `PermissionToStart.schema.json` (Unknown)
--- src/streamConsumer.ts
+++
@@ -1,6 +0,0 @@
-import { SalesforcePortalClient } from './SalesforcePortalClient'
-import './lib/env'
-
-const sfClient =  new SalesforcePortalClient(process.env.SALESFORCE_CONSUMER_KEY!, process.env.SALESFORCE_CONSUMER_SECRET!, process.env.SALESFORCE_USERNAME!, process.env.SALESFORCE_PASSWORD!)
-sfClient.subscribeToChannel('/topic/CaseUpdates')
-// sfClient.subscribeToChannel('/data/ChangeEvents')
