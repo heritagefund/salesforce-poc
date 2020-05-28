0879ae79905e11ea764c00c1c2d297aca780cb58
Stuart McColl <7103229+stuartmccoll@users.noreply.github.com>
2020-05-28 14:18:30 +0100

    Cleanup of src/index.ts for readability
    This commit contains some small changes to the src/index.ts
    file to increase readability.

--- src/index.ts
+++ src/index.ts
@@ -6,37 +6,57 @@ import * as inquirer from 'inquirer'
 import uuid = require('uuid')
 
 
-
 class PortalCli extends Command {
   async run() {
-    const sfClient = new SalesforcePortalClient(process.env.SALESFORCE_CONSUMER_KEY!, process.env.SALESFORCE_CONSUMER_SECRET!, process.env.SALESFORCE_USERNAME!, process.env.SALESFORCE_PASSWORD!)
 
-    this.log('hello world')
-    this.log('welcome to the Portal 🏛')
-    this.log('=========================')
-    const promptNewOrExisting = await inquirer.prompt([{
-      name: 'newOrExisting',
+    const sfClient = new SalesforcePortalClient(
+      process.env.SALESFORCE_CONSUMER_KEY!, 
+      process.env.SALESFORCE_CONSUMER_SECRET!, 
+      process.env.SALESFORCE_USERNAME!, 
+      process.env.SALESFORCE_PASSWORD!
+    )
+
+    this.log('National Lottery Heritage Fund')
+    this.log('Test Client for Salesforce Integration')
+    this.log('--------------------------------------')
+
+    const promptSubmissionType = await inquirer.prompt([{
+      name: 'submissionType',
       type: 'list',
       choices: [{ name: 'new' }, { name: 'existing' }]
     }])
-    const applicationId = await promptNewOrExisting.newOrExisting === 'existing' ? await cli.prompt('applicationId') : uuid()
+
+    const applicationId = await promptSubmissionType.submissionType === 'existing' ? await cli.prompt('applicationId') : uuid()
+
     const promptFormType = await inquirer.prompt([{
       name: 'formType',
       type: 'list',
       choices: [{ name: '3-10k-grant' }, { name: 'permission-to-start' }, {name: 'completion-report'}]
     }])
+
     let projectTitle
     let organisationName
+
     if (promptFormType.formType === '3-10k-grant') {
       projectTitle = await cli.prompt('Project title')
       organisationName = await cli.prompt('Organisation name')
     }
 
+    await Promise.all(
+      [
+        sfClient.getInstanceId(), 
+        sfClient.postApexRest(
+          promptFormType.formType, 
+          applicationId, 
+          projectTitle, 
+          organisationName,
+          )
+      ]
+    ).then(
+      // Log out the payload which is being sent via a HTTP POST request to Salesforce
+      p => this.log("p[1]")
+    )
 
-
-    // await Promise.all([sfClient.getInstanceId(), sfClient.postApexRest(projectTitle, organisationName, formTypeEnum)]).then(p => this.log(`${p[0]}/${JSON.parse(p[1]).caseId}`))
-    console.log(promptFormType.formType)
-    await Promise.all([sfClient.getInstanceId(), sfClient.postApexRest(promptFormType.formType, applicationId, projectTitle, organisationName,)]).then(p => this.log(p[1]))
   }
 }
 
